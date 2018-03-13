79c3c8b329e80f877b06081f48eb667d92045564
Chris Gwilliams <encima@gmail.com>
2018-03-13 13:23:27 +0100

    escape quotes in exec

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 
 git init
 git config user.name "Chris Gwilliams"
 git config user.email "encima@gmail.com"
 
 git remote add upstream "https://encima:$GH_TOKEN@github.com/SciCatProject/docs.git"
-
+git pull origin upstream
 git checkout gh-pages
 git merge master
 ./node_modules/gitbook-cli/bin/gitbook.js build . docs
-rm -rf !("docs")
+exec `rm -rf '("docs")`
 mv docs/* .
 rm -rf docs
 
