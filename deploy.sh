4743a74b33b3eb67ee24146dc1c440f29fd58240
Chris Gwilliams <encima@gmail.com>
2018-03-13 12:22:02 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,15 @@ git init
 git config user.name "Chris Gwilliams"
 git config user.email "encima@gmail.com"
 
-git remote add upstream "https://$GH_TOKEN@github.com/SciCatProject/docs.git"
-git fetch upstream
+git remote add upstream "https://encima:$GH_TOKEN@github.com/SciCatProject/docs.git"
 
-git add -A .
+git checkout gh-pages
+git merge master
+./node_modules/gitbook-cli/bin/gitbook.js build . docs
+rm -rf !("docs")
+mv docs/* .
+rm -rf docs
+
+git add -a .
 git commit -m "rebuild pages at ${rev}"
-git push upstream master
+git push upstream gh-pages
