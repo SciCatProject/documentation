619cd23ab9799dd7f87dbdb2902b1ae7dd7a52d5
egli <stephan.egli@psi.ch>
2020-09-29 11:11:39 +0200

    Removed docs contents

--- docs/fetch_docs_from_catanie.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-git init cat
-cd cat
-git remote add origin https://github.com/ScicatProject/catanie
-git config core.sparsecheckout true
-echo "documentation/*" >> .git/info/sparse-checkout
-git pull --depth=1 origin master
-ls 
